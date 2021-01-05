a708ae508710241637b5b0672e179e635c7d6520
Taina Conde <tainabatistaconde@gmail.com>
2021-01-05 17:39:42 -0600

    refactor: App is class component now

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
+import React, {Component} from 'react';
 import './App.css';
 import { Route } from 'react-router-dom';
 import InitialMenu from './Components/InitialMenu';
 import PlayBtn from './Components/PlayBtn';
 import Game from './Components/Game';
 
-function App() {
-  return (
-    <div className="App">
-      
-      <Route
-        exact path = "/"
-        render = {() => (
-          <div>
-            <InitialMenu/>
-            <PlayBtn/>
-          </div>
-        )}/>
-      <Route
-        path = "/game"
-        render = {() => (
-          <div>
-            <Game/>
-          </div>
-        )}  
-      />
-      <Route/>
-      <Route/>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Route
+          exact path = "/"
+          render = {() => (
+            <div>
+              <InitialMenu/>
+              <PlayBtn/>
+            </div>
+          )}/>
+        <Route
+          path = "/game"
+          render = {() => (
+            <div>
+              <Game/>
+            </div>
+          )}  
+        />
+        <Route/>
+        <Route/>
+      </div>
+    );
+  }
 }
 
 export default App;
