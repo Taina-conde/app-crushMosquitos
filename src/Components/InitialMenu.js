af310284e8763c2e423a5345ecf7201abbe2ee14
Taina Conde <tainabatistaconde@gmail.com>
2021-01-05 17:19:03 -0600

    style: Level of Difficulty
    Add prepend label in difficulty level selector and add a few divs to
    style according to Bootstrap forms.

--- src/Components/InitialMenu.js
+++ src/Components/InitialMenu.js
@@ -20,25 +20,32 @@ class InitialMenu extends Component {
                 <div className = "row">
                     <form>
                         <div className = "form-group">
-                            <select 
-                                className ="form-control" 
-                                id="difficultyLevel"
-                                value = {this.state.value}
-                                onChange = {(event) => this.handleSelect(event.target.value) }
-                            >
-                                <option value="level">
-                                    --Level of difficulty--
-                                </option>
-                                <option value="easy">
-                                    Easy
-                                </option>
-                                <option value="normal">
-                                    Normal
-                                </option>
-                                <option value="hard">
-                                    Hard
-                                </option>
-                            </select>
+                            <div className = "input-group">
+                                <div className = "input-group-prepend">
+                                    <label className = "input-group-text" for = "difficultyLevel">Level of difficulty</label>
+                                </div>
+                                <select 
+                                    className ="form-control" 
+                                    id="difficultyLevel"
+                                    value = {this.state.value}
+                                    onChange = {(event) => this.handleSelect(event.target.value) }
+                                >
+                                    <option value="level">
+                                        --choose--
+                                    </option>
+                                    <option value="easy">
+                                        Easy
+                                    </option>
+                                    <option value="normal">
+                                        Normal
+                                    </option>
+                                    <option value="hard">
+                                        Hard
+                                    </option>
+                                </select>
+                            
+                            </div>
+                            
                             
 
                         </div>
