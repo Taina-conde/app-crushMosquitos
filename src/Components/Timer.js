f6ecc07958820db5e8f5eea5a48d9efb608dd0e7
Taina Conde <tainabatistaconde@gmail.com>
2021-01-07 17:10:58 -0600

    feat: Set timer and redirect
    Add redirect to state. Add second argument to setState to clear
    the timer when reaches 0 and set the state's redirect prop to true.
    Inside the render method, use Redirect to set the path to victory.

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -1,18 +1,29 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 
 
 class Timer extends React.Component {
     state = {
+        redirect: false,
         time: 15
     }
     componentDidMount () {
-        setInterval( 
+        const timer = setInterval( 
                 () => {
-                    this.setState({time: this.state.time -1})
+                    this.setState({time: this.state.time -1}, () =>{
+                        if (this.state.time === 0) {
+                            clearInterval(timer);
+                            this.setState({redirect: true});
+                        }
+                    })
                     }, 
             1000)
+       
     }
     render() {
+        if (this.state.redirect === true ) {
+            return <Redirect to = '/victory'/>
+        }
         return (
             <div>
                 Time remaining: {this.state.time > 0 && this.state.time}
