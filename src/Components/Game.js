b8dafd397ccf0d789162144513930e96eacbbd6d
Taina Conde <tainabatistaconde@gmail.com>
2021-01-07 18:22:27 -0600

    feat: Create Mosquito component
    Create Mosquito file and add the component to Game. Mosquito img is
    not working.

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
 import GamePanel from './GamePanel';
+import Mosquito from './Mosquito';
 
 class Game extends Component {
     render() {
         return (
             <div>
+                <Mosquito
+                  difficulty = {this.props.difficulty}
+                />
 
                 <GamePanel/>
 
---
+++ src/Components/Mosquito.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Mosquito(props) {
+    return(
+        <div >
+            <img className = 'img-fluid' src = {'../images/mosquito.png'} alt = 'mosquito'/>
+        </div>
+    )
+}
+export default Mosquito;
