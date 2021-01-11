c6fc7c1a6bfc72956084a18ac91a6efed02a6278
Taina Conde <tainabatistaconde@gmail.com>
2021-01-11 17:47:04 -0600

    fix: Add alt to img in Lives

--- src/Components/Lives.js
+++ src/Components/Lives.js
@@ -8,30 +8,30 @@ function Lives(props) {
         <div className = "lives">
             {props.lives === 3 && (
                 <div>
-                    <img src={fullHeart}/>
-			        <img src={fullHeart}/>
-			        <img src={fullHeart}/>
+                    <img src={fullHeart} alt= 'hf1'/>
+			        <img src={fullHeart} alt= 'hf2'/>
+			        <img src={fullHeart} alt= 'hf3'/>
                 </div>
             )}
             {props.lives === 2 && (
                 <div>
-                    <img src={fullHeart}/>
-			        <img src={fullHeart}/>
-			        <img src={emptyHeart}/>
+                    <img src={fullHeart} alt= 'hf1'/>
+			        <img src={fullHeart} alt= 'hf2'/>
+			        <img src={emptyHeart} alt= 'he3'/>
                 </div>
             )}
             {props.lives === 1 && (
                 <div>
-                    <img src={fullHeart}/>
-			        <img src={emptyHeart}/>
-			        <img src={emptyHeart}/>
+                    <img src={fullHeart} alt= 'hf1'/>
+			        <img src={emptyHeart} alt= 'he2'/>
+			        <img src={emptyHeart} alt= 'he3'/>
                 </div>
             )}
             {props.lives === 0 && (
                 <div>
-                    <img src={emptyHeart}/>
-			        <img src={emptyHeart}/>
-			        <img src={emptyHeart}/>
+                    <img src={emptyHeart} alt= 'he1'/>
+			        <img src={emptyHeart} alt= 'he2'/>
+			        <img src={emptyHeart} alt= 'he3'/>
                 </div>
             )}
             
