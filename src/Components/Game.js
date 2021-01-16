399cef34360659c22ede9a972b0d8cc52fa3f808
Taina Conde <tainabatistaconde@gmail.com>
2021-01-15 18:30:57 -0600

    feat: Mosquito interval
    Define x and y positions to create the Mosquito in
    random positions in the window. Build createMosquito interval
    inside componentDidMount, to which the bug might be related.

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,16 +1,75 @@
 import React, {Component} from 'react';
 import GamePanel from './GamePanel';
 import Mosquito from './Mosquito';
+import { Redirect } from 'react-router-dom';
 
 class Game extends Component {
+    state = {
+        mosquitoTime: [
+            {
+              id: "easy",
+              time: 2000
+            },
+            {
+              id: "normal",
+              time: 1300
+            },
+            {
+              id: "hard",
+              time: 900
+            }
+        ],
+        mosquitoExists: false,
+        redirect: false
+    }
+    componentDidMount() {
+        const { difficulty } = this.props
+        const selectedLevel = this.state.mosquitoTime.filter((diffLevel) => difficulty === diffLevel.id)
+        const time = selectedLevel.time
+        
+        const createMosquito = setInterval (
+            () => {
+                this.setState({
+                    mosquitoExists: !this.state.mosquitoExists
+                }, () => {
+                    if (this.props.lives === 0 ){
+                        clearInterval(createMosquito)
+                        this.setState({
+                            redirect: true
+                        })
+                    }
+                })
+            }, time
+        )
+    }
+    
     render() {
+        
+        //TODO: define height and width depending on the size of the window
+        const height = 1000
+        const width = 1000
+        let x = Math.floor(Math.random()*width) //- 90 
+        let y = Math.floor(Math.random()*height) //- 90
+        // x = x < 0 ? 0 : x
+        // y = y < 0 ? 0 : y
+        const styles = {
+            left: x,
+            top: y
+        }
+        if (this.state.redirect === true ) {
+            return <Redirect to = '/gameOver'/>
+        }
+        
         return (
+           
             <div>
-                <Mosquito
-                  difficulty = {this.props.difficulty}
-                  onClickMosquito = {this.props.onClickMosquito}
-                />
-
+                {this.state.mosquitoExists && 
+                    <Mosquito
+                        difficulty = {this.props.difficulty}
+                        onClickMosquito = {this.props.onClickMosquito}
+                        style = {styles}
+                    />
+                }
                 <GamePanel
                     lives = {this.props.lives}
                 />
