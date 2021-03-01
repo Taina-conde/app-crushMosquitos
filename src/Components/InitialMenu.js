5ad6bb243380f434d036989ab973ac46ed232a35
Taina Conde <tainabatistaconde@gmail.com>
2021-03-01 17:51:18 -0500

    style: InitialMenuv - delete col class from elements in flexbox

--- src/Components/InitialMenu.js
+++ src/Components/InitialMenu.js
@@ -6,20 +6,20 @@ export default function InitialMenu(props) {
 
         <div className = 'col d-flex flex-column'>
             {props.title &&
-                <div className = 'col' >
-                    <h1 className = "col display-1 text-white">{props.title}</h1>
+                <div >
+                    <h1 className = "display-1 text-white">{props.title}</h1>
                 </div>
             }
             
             {props.image && 
-                <div className = "col justify-content-center">
+                <div >
                     <img className = 'img-fluid' src = {gameOver} alt = 'gameOver'/>
                 </div>     
             }
 
             {props.subtitle &&
-                <div className = 'col'>
-                    <h1 className = "col display-6 text-white">{props.subtitle}</h1>
+                <div >
+                    <h1 className = "display-6 text-white">{props.subtitle}</h1>
                 </div>
             }
         </div>
--- src/Components/PlayBtn.js
+++ src/Components/PlayBtn.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function PlayBtn(props) {
     const { difficulty } = props
     return (
-        <div>
+        <div className = 'col'>
             <Link to = "/game">
                 <button 
                     type= "button"
