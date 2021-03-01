f6ff779dc705b1c20689e2c1981739f375891075
Taina Conde <tainabatistaconde@gmail.com>
2021-03-01 17:46:34 -0500

    style: InitialMenu - add col and d-flex classes
    Add col class to outer div and make it a flexbox by adding
    d-flex class.

--- src/Components/InitialMenu.js
+++ src/Components/InitialMenu.js
@@ -4,21 +4,21 @@ import gameOver from '../images/game_over.png'
 export default function InitialMenu(props) {
     return(
 
-        <div className = 'container'>
+        <div className = 'col d-flex flex-column'>
             {props.title &&
-                <div className = "row">
+                <div className = 'col' >
                     <h1 className = "col display-1 text-white">{props.title}</h1>
                 </div>
             }
             
             {props.image && 
-                <div className = "row justify-content-center">
+                <div className = "col justify-content-center">
                     <img className = 'img-fluid' src = {gameOver} alt = 'gameOver'/>
                 </div>     
             }
 
             {props.subtitle &&
-                <div className = 'row'>
+                <div className = 'col'>
                     <h1 className = "col display-6 text-white">{props.subtitle}</h1>
                 </div>
             }
