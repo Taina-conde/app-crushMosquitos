b69e9679290970c4885b22dd2b85448a92df52e2
Taina Conde <tainabatistaconde@gmail.com>
2021-04-28 18:16:49 -0400

    feat: GameContextProvider
    Create component that wraps the whole application
    and sends the necessary information to other
    components.

--- src/store/game-context.js
+++ src/store/game-context.js
@@ -1,27 +1,68 @@
-import { createContext, useState } from 'react';
+import { createContext, useState } from "react";
 
 const GameContext = createContext({
-    difficulty: "",
-    lives: 3,
-    isCrushed: false,
-    mosquitoTime: [
-        {
-          id: "easy",
-          time: 2000
-        },
-        {
-          id: "normal",
-          time: 1300
-        },
-        {
-          id: "hard",
-          time: 900
-        }
-    ],
-    mosquitoExists: false,
-    selectDifficulty : (diff) => {},
-    clickMosquito : () => {},
-    toggleMosquito : () => {},
-    loseLives: () => {}
+  difficulty: "",
+  lives: 3,
+  isCrushed: false,
+  mosquitoExists: false,
+  mosquitoTime: [
+    {
+      id: "easy",
+      time: 2000,
+    },
+    {
+      id: "normal",
+      time: 1300,
+    },
+    {
+      id: "hard",
+      time: 900,
+    },
+  ],
+  selectDifficulty: (diff) => {},
+  clickMosquito: () => {},
+  toggMosquitoExists: () => {},
+  loseLives: () => {},
 });
 
+export function GameContextProvider(props) {
+  const [difficulty, setDifficulty] = useState("");
+  const [lives, setNumLives] = useState(3);
+  const [isCrushed, setIsCrused] = useState(false);
+  const [mosquitoExists, setMosquitoExists] = useState(false);
+
+  function selectDifficultyHandler(selectedDifficulty) {
+    setDifficulty(selectedDifficulty);
+  }
+  function loseLivesHandler() {
+    setNumLives((previousNumLives) => {
+      return previousNumLives - 1;
+    });
+  }
+  function clickMosquitoHandler() {
+    setIsCrused(true);
+  }
+  function toggleMosquitoExistsHandler() {
+    setMosquitoExists((previousMosquitoExists) => {
+      return !previousMosquitoExists;
+    });
+  }
+  const context = {
+    difficulty,
+    lives,
+    isCrushed,
+    mosquitoExists,
+    selectDifficulty: selectDifficultyHandler,
+    clickMosquito: clickMosquitoHandler,
+    toggMosquitoExists: toggleMosquitoExistsHandler,
+    loseLives: loseLivesHandler,
+  };
+
+  return (
+    <GameContext.Provider value={context}>
+      {props.children}
+    </GameContext.Provider>
+  );
+}
+
+export default GameContext;
